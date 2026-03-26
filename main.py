fe8a9c7feded1e81b0013a8ed0005e440555dce3
glendatzori <glendatzori@gmail.com>
2026-03-26 09:37:16 +0100

    add requirements

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates") #dobbiamo dire al motore di templating dove sono contenuti i nostri file di templates
+#meccanismo che ci permette di prendere il codice che abbiamo scritto e metterlo in output si chiama TEMPLATING
 
-# per ogni n point affiungiamo una finzione python
-@app.get("/")  # n point, richiesta get, tra parentesi il percorso
-def hello_world():
-    return "Hello World!"
+"""
+@app.get("/", response_class=HTMLResponse)  #formato in cui fastapi deve restituire il valore della funzione ---> statico
+def home(request: Request):
+    * Renders the home page. *
+    return templates.TemplateResponse(
+        request=request, name='home.html'  #quale file html passare al sito (in questo caso home o base)
+    ) # questo pattern verrà ripetuto
+"""
 
-# 127.0.0.1 è URL del nostro computer (local host)
+@app.get("/", response_class=HTMLResponse)  #formato in cui fastapi deve restituire il valore della funzione ---> statico
+def home(request: Request):
+    text = {
+        "title" : "Home page",
+        "content" : "Welcome to the home page!"
+    }
+    context = {
+        "text" : text,
+        "sequence" : ["a", "b", "c"]
+    }
+    return templates.TemplateResponse(
+        request=request,
+        name='home.html',
+        context=context
+    )
 
-# @app.get("/hello")
-# def hello_world():
-#     return "Hello World!"
