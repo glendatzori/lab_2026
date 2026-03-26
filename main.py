86a084d64cbd38f4eda3aff29be5e06144a03e9b
glendatzori <glendatzori@gmail.com>
2026-03-26 18:35:43 +0100

    add requirements

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory = "static"), name="static")
 templates = Jinja2Templates(directory="templates") #dobbiamo dire al motore di templating dove sono contenuti i nostri file di templates
 #meccanismo che ci permette di prendere il codice che abbiamo scritto e metterlo in output si chiama TEMPLATING
-
-"""
+product_list = [
+    { "name":"notebook DELL", "price": 2999.99, "location": "Cagliari"},
+    { "name":"smartphone Xiaomi", "price": 999.99, "location": "Napoli"},
+    { "name":"notebook ASUS", "price": 1599.99, "location": "Bari"}
+]
 @app.get("/", response_class=HTMLResponse)  #formato in cui fastapi deve restituire il valore della funzione ---> statico
 def home(request: Request):
-    * Renders the home page. *
-    return templates.TemplateResponse(
-        request=request, name='home.html'  #quale file html passare al sito (in questo caso home o base)
-    ) # questo pattern verrà ripetuto
-"""
 
-@app.get("/", response_class=HTMLResponse)  #formato in cui fastapi deve restituire il valore della funzione ---> statico
-def home(request: Request):
-    text = {
-        "title" : "Home page",
-        "content" : "Welcome to the home page!"
-    }
-    context = {
-        "text" : text,
-        "sequence" : ["a", "b", "c"]
-    }
     return templates.TemplateResponse(
         request=request,
         name='home.html',
-        context=context
+        context={"text": "Welcome to the store"}
+    )
+@app.get("/products", response_class=HTMLResponse)
+def products(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name='products.html',
+        context={"product_list": product_list}
     )
 
+"""
+@app.get("/ciao")
+def pippo
+url_for('pippo') ->"/ciao"
+"""
